10695b3c772fd255e156fa7d3d7b9fb36ef3d6ac
Kelly Milligan <kelly.milligan@gmail.com>
2013-05-22 14:26:33 -0500

    removing looping through properties for the time being...
    preliminary tests show that this is not benefitting the frameworks
    memory footprint.

--- src/www/js/Lavaca/util/Disposable.js
+++ src/www/js/Lavaca/util/Disposable.js
@@ -2,31 +2,6 @@ define(function(require) {
 
   var extend = require('./extend');
 
-  function _disposeOf(obj) {
-    var n,
-        o,
-        i;
-    for (n in obj) {
-      if (obj.hasOwnProperty(n)) {
-        o = obj[n];
-        if (o) {
-          if (typeof o.dispose === 'function') {
-            o.dispose();
-          } else if (o instanceof Array) {
-            for (i = o.length - 1; i > -1; i--) {
-              if (o[i] && typeof o[i].dispose === 'function') {
-                o[i].dispose();
-              } else {
-                _disposeOf(o[i]);
-              }
-            }
-          }
-        }
-      }
-      delete obj[n];
-    }
-  }
-
   /**
    * @class Lavaca.util.Disposable
    * Abstract type for types that need to ready themselves for GC
@@ -39,7 +14,6 @@ define(function(require) {
      * Readies the object to be garbage collected
      */
     dispose: function() {
-      _disposeOf(this);
     }
   });
 
