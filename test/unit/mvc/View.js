10836f6113eb75b46d8775afe080b6c2aff237a4
Jake Riesterer <jake.riesterer@mutualmobile.com>
2013-05-24 13:52:54 -0500

    Add tests for View.mapEvent #85

--- test/unit/mvc/View.js
+++ test/unit/mvc/View.js
@@ -7,16 +7,20 @@ define(function(require) {
 
   describe('A View', function() {
     var testView,
-        el = $('<div/>'),
+        el,
         model;
     beforeEach(function() {
-      $('body').append('<script type="text/dust-template" data-name="hello-world"><form>Hello World <input type="text"><div class="button">Button</div></form></script>');
+      el = $('<div/>');
+      $('body')
+        .append('<script type="text/dust-template" data-name="hello-world"><form>Hello World <input type="text"><div class="button">Button</div></form></script>')
+        .append(el);
       model = new Model({color: 'blue', primary: true});
       Template.init();
       testView = new View();
     });
     afterEach(function() {
       $('script[data-name="hello-world"]').remove();
+      el.remove();
       testView.dispose();
     });
     it('can be initialized', function() {
@@ -145,6 +149,89 @@ define(function(require) {
       });
       $('script[data-name="model-tmpl"]').remove();
     });
-  });
+    describe('can map events', function() {
+      var initWithEventMap = function(map, callback) {
+            var myView;
+            runs(function() {
+              var MyView = View.extend(function MyView() {
+                    View.apply(this, arguments);
+                    this.mapEvent(map);
+                  }, {
+                    template: 'hello-world',
+                    onRenderSuccess: function() {
+                      View.prototype.onRenderSuccess.apply(this, arguments);
+                      setTimeout(function() {
+                        renderSuccesful = true;
+                      });
+                    }
+                  });
+              myView = new MyView(el, model);
+              myView.render();
+            });
+            waitsFor(function() {
+              return !!renderSuccesful;
+            });
+            runs(function() {
+              callback(myView);
+            });
+          },
+          renderSuccesful;
 
+      beforeEach(function() {
+        renderSuccesful = false;
+      });
+
+
+      it('to it\'s own el', function() {
+        var clickHandler = jasmine.createSpy('clickHandler');
+        initWithEventMap({
+          self: {
+            click: clickHandler
+          }
+        }, function(myView) {
+          myView.el.trigger('click');
+          expect(clickHandler.callCount).toBe(1);
+          myView.dispose();
+        });
+      });
+      it('to a child of it\'s el', function() {
+        var formHandler = jasmine.createSpy('formHandler'),
+            buttonHandler = jasmine.createSpy('buttonHandler');
+        initWithEventMap({
+          'form': {
+            click: formHandler
+          },
+          '.button': {
+            click: buttonHandler
+          }
+        }, function(myView) {
+          myView.el.find('form').trigger('click');
+          expect(formHandler.callCount).toBe(1);
+          expect(buttonHandler.callCount).toBe(0);
+          myView.dispose();
+        });
+      });
+      it('to it\'s model', function() {
+        var changeHandler = jasmine.createSpy('changeHandler'),
+            myPropHandler = jasmine.createSpy('myPropHandler'),
+            otherPropHandler = jasmine.createSpy('otherPropHandler');
+        initWithEventMap({
+          model: {
+            'change': changeHandler,
+            'change.myProp': myPropHandler,
+            'change.otherProp': otherPropHandler
+          }
+        }, function(myView) {
+          model.set('myProp', 'test');
+          // Make sure model event handlers are
+          // removed when the view is disposed
+          myView.dispose();
+          model.set('myProp', 'test2');
+          expect(changeHandler.callCount).toBe(1);
+          expect(myPropHandler.callCount).toBe(1);
+          expect(otherPropHandler.callCount).toBe(0);
+        });
+      });
+    });
+  });
 });
