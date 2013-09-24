69cbd312581ff40dfbaf309942fa564e44d537a6
George Henderson <georgehendersonmusic@gmail.com>
2013-09-24 09:37:02 -0500

    substituted nom for bower to handle package management for client side code.

--- src/boot.js
+++ src/boot.js
@@ -1,16 +1,16 @@
 require.config({
   baseUrl: 'js',
   paths: {
-    'es5-shim': 'libs/es5-shim',
-    '$': 'libs/jquery-2.0.0',
-    'jquery': 'libs/jquery-2.0.0',
+    'es5-shim': '../components/es5-shim/es5-shim',
+    '$': '../components/jquery/index',
+    'jquery': '../components/jquery/index',
     'cordova': 'libs/cordova',
-    'mout': 'libs/mout',
+    'mout': '../components/mout/src',
     'docCookies': 'libs/docCookies',
-    'dust': 'libs/dust-full-2.0.3',
-    'dust-helpers': 'libs/dust-helpers-1.1.1',
+    'dust': '../components/dustjs-linkedin/dist/dust-full-2.0.3',
+    'dust-helpers': '../components/dustjs-linkedin-helpers/dist/dust-helpers-1.1.1',
     'rdust': 'libs/require-dust',
-    'iscroll': 'libs/iscroll-lite',
+    'iscroll': '../components/iscroll/dist/iscroll-lite-min',
     'lavaca': 'lavaca'
   },
   shim: {
