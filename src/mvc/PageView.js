8e1b04f7ea4b7d88f2bfe356aadf0541e5e12fe0
Zach Shipley <zach@zachshipley.com>
2014-07-31 11:40:13 -0500

    Issue #141: remove mvc/PageView

--- src/mvc/PageView.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-
-  console.log('WARNING: PageView is deprecated. You can now use the normal View class in place of PageView.');
-
-  var View = require('./View');
-  return View;
-
-});
